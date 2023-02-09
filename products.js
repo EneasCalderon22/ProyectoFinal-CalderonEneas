220d293ac91a1287609b1097cee39aa9a90fc1c9
EneasCalderon22 <eneas_calderon@yahoo.com>
2023-02-09 17:41:05 -0300

    first

---
+++ products.js
@@ -0,0 +1,75 @@
+const productos = [
+    {
+        id:1,
+        nombre:"Galletas",
+        precio:95,
+        img:"https://www.paulinacocina.net/wp-content/uploads/2022/04/galletas-de-chocolate-receta.jpg"
+    },
+    {
+        id:2,
+        nombre:"Cerveza",
+        precio:100,
+        img:"https://www.clarin.com/img/2022/08/04/dia-internacional-de-la-cerveza___ZOge84kZg_2000x1500__1.jpg"
+    },
+    {
+        id:3,
+        nombre:"Pizza",
+        precio:140,
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg"
+    },
+    {
+        id:4,
+        nombre:"Papas con Cheddar",
+        precio:120,
+        img:"https://live.mrf.io/statics/i/ps/irecetasfaciles.com/wp-content/uploads/2019/05/papas-francesas-con-tocineta-y-salsa-de-queso.png?width=1200&enable=upscale"
+    },
+    {
+        id:5,
+        nombre:"Hamburguesa Gigante",
+        precio:200,
+        img:"https://media.gqitalia.it/photos/5d56b18eee3934000869d3a0/1:1/w_3142,h_3142,c_limit/GettyImages-1032835444.jpg"
+    },
+    {
+        id:6,
+        nombre:"Rabas",
+        precio:180,
+        img:"https://vinomanos.com/wp-content/uploads/2021/01/Rabas.jpg"
+    },
+    {
+        id:7,
+        nombre:"Tacos",
+        precio:160,
+        img:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/birria-tacos-1648842127.jpg"
+    },
+    {
+        id:8,
+        nombre:"Hot Dogs",
+        precio:90,
+        img:"https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/newscms/2020_27/1586837/hotdogs-te-main-200702.jpg"
+    },
+    {
+        id:9,
+        nombre:"Empanadas X6",
+        precio:120,
+        img:"https://www.goya.com/media/7607/chipotle-chicken-empanadas.jpg?quality=80"
+    },
+];
+
+
+// const getDatos = () => {
+//     return datos;
+// }
+
+// const getDatos = () => {
+//     return new Promise ((resolve, reject) => {
+//         setTimeout(()
+//     => {
+//         resolve(datos);
+//     }, 1500);
+//     })
+// }
+
+// getDatos() 
+// .then((datos) => console.log(datos));
+
+
